adbb67320c357ef54de76937f50ecbb50307c7c8
Alexandre Courouble <acourouble@vmware.com>
2019-06-22 10:22:17 -0700

    Creates pkg/ subdirectory and implements utils package
    Signed-off-by: Alexandre Courouble <acourouble@vmware.com>

--- cmd/profile/list/list.go
+++ cmd/profile/list/list.go
@@ -23,6 +23,7 @@ import (
 	"text/tabwriter"
 	"time"
 
+	"github.com/edgexfoundry/edgex-cli/pkg/utils"
 	models "github.com/edgexfoundry/go-mod-core-contracts/models"
 	"github.com/spf13/cobra"
 )
@@ -64,8 +65,8 @@ func NewCommand() *cobra.Command {
 				fmt.Fprintf(w, "%s\t%s\t%v\t%v\t%v\t%v\t\n",
 					device.Id,
 					device.Name,
-					humanDuration(time.Since(tCreated)),
-					humanDuration(time.Since(tModified)),
+					utils.HumanDuration(time.Since(tCreated)),
+					utils.HumanDuration(time.Since(tModified)),
 					device.Manufacturer,
 					device.Model,
 				)
@@ -74,30 +75,4 @@ func NewCommand() *cobra.Command {
 		},
 	}
 	return cmd
-}
-
-// Taken from https://github.com/docker/go-units/blob/master/duration.go
-func humanDuration(d time.Duration) string {
-	if seconds := int(d.Seconds()); seconds < 1 {
-		return "Less than a second"
-	} else if seconds == 1 {
-		return "1 second"
-	} else if seconds < 60 {
-		return fmt.Sprintf("%d seconds", seconds)
-	} else if minutes := int(d.Minutes()); minutes == 1 {
-		return "About a minute"
-	} else if minutes < 60 {
-		return fmt.Sprintf("%d minutes", minutes)
-	} else if hours := int(d.Hours() + 0.5); hours == 1 {
-		return "About an hour"
-	} else if hours < 48 {
-		return fmt.Sprintf("%d hours", hours)
-	} else if hours < 24*7*2 {
-		return fmt.Sprintf("%d days", hours/24)
-	} else if hours < 24*30*2 {
-		return fmt.Sprintf("%d weeks", hours/24/7)
-	} else if hours < 24*365*2 {
-		return fmt.Sprintf("%d months", hours/24/30)
-	}
-	return fmt.Sprintf("%d years", int(d.Hours())/24/365)
 }
